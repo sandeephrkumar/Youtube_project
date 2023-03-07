3f3af3edc74e5ca4a3fce5be4c71e48bdaf6ff86
Sandeep personal <sandeephrkumar@gmail.com>
2023-03-07 16:05:34 +0530

    added train test split

--- templ.py
+++ templ.py
@@ -3,3 +3,20 @@ print("content for git sandeep local")
 # added additional comment 
 for i in range(10):
     print(i)
+from sklearn.datasets import load_iris
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+data = load_iris()
+
+
+X = data.data
+y = data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+le = preprocessing.LabelEncoder()
+
+
+
+
+
+
